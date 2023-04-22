52307ccd2e06b109866a6945c7afad06523bd46a
enfff <enforcetitan@gmail.com>
2023-04-22 17:18:34 +0200

    we understood what the hell a GATT is

--- bluetooth/astruino_ble.py
+++ bluetooth/astruino_ble.py
@@ -2,20 +2,23 @@ import asyncio
 # import time as t
 from bleak import BleakScanner, BleakClient
 
-mac_address="01:23:45:67:A6:31"
+mac_address = "01:23:45:67:A6:31"
+VENDOR_SPECIFIC_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
 
 async def main():
     # devices = await BleakScanner.discover()
     # for d in devices:
     #     print(d)
 
-    astruino = await BleakScanner.find_device_by_name('astruino', timetout=5.0);
+
+
+    astruino = await BleakScanner.find_device_by_name('astruino', timetout=5.0)
     if astruino:
         print(astruino)
-    else:''
+    else:
         print("non funziona yolo")
         return
-    
+
     # Collegato e funzionante :)
 
     async with BleakClient(mac_address) as client:
@@ -26,15 +29,14 @@ async def main():
             await client.connect()
             print("astruino connesso!")
 
-        # non so cosa sia
-        # https://bleak.readthedocs.io/en/latest/api/client.html
-        char_specifier = {client.services, }     
-        data = {}
-        response = False;           
-        await client.read_gatt_char(char_specifier: [client.services, ]);
+        ##
 
-        # t.sleep(20);
+        res_bytes = await client.read_gatt_char(VENDOR_SPECIFIC_UUID)
+        res = bytearray.decode(res_bytes)
+        print(res)
+
+        # t.sleep(5);
         await client.disconnect()
-        print("astruino sconnesso!")
+    print("astruino sconnesso!")
 
 asyncio.run(main())
