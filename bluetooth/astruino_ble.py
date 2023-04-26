575c800d02d9987a070863e4d2b8a9b48b5e2705
enfff <enforcetitan@gmail.com>
2023-04-26 23:24:56 +0200

    first message sent

--- bluetooth/astruino_ble.py
+++ bluetooth/astruino_ble.py
@@ -4,13 +4,13 @@ from bleak import BleakScanner, BleakClient
 
 mac_address = "01:23:45:67:A6:31"
 VENDOR_SPECIFIC_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
+IO_CONFIG_CHAR_UUID = "f000aa66-0451-4000-b000-000000000000"
+
 
 async def main():
     # devices = await BleakScanner.discover()
     # for d in devices:
     #     print(d)
-
-
 
     astruino = await BleakScanner.find_device_by_name('astruino', timetout=5.0)
     if astruino:
@@ -31,11 +31,18 @@ async def main():
 
         ##
 
+        for service in client.services:
+            print(service)
+            for char in service.characteristics:
+                print("\t\t", char)
+
+        await client.write_gatt_char(VENDOR_SPECIFIC_UUID, b"forza napoli")
+
         res_bytes = await client.read_gatt_char(VENDOR_SPECIFIC_UUID)
         res = bytearray.decode(res_bytes)
         print(res)
 
-        # t.sleep(5);
+        t.sleep(5)
         await client.disconnect()
     print("astruino sconnesso!")
 
